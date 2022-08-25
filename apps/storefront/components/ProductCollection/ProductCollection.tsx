e97a2961d3f9794e0217f1a4250bcd02778164e6
Alen Ichshanov <a.ichshanov@adamar.solutions>
2022-08-25 17:05:36 +0600

    fix: product collection cards pos

--- apps/storefront/components/ProductCollection/ProductCollection.tsx
+++ apps/storefront/components/ProductCollection/ProductCollection.tsx
@@ -85,7 +85,7 @@ export const ProductCollection = ({
   return (
     <div>
       <ul
-        className={`grid gap-2 ${mainView ? "grid-cols-2" : "grid-cols-4"}`}
+        className={`grid gap-2 ${mainView ? "grid-cols-2" : "grid-cols-1"}`}
         data-testid="productsList"
       >
         {products.map((product) => (
