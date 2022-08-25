ea368c5565c92cf50936841be94b8474e9d46a50
Alen Ichshanov <a.ichshanov@adamar.solutions>
2022-08-25 17:16:28 +0600

    fix: zoom disabled

--- apps/storefront/pages/_document.tsx
+++ apps/storefront/pages/_document.tsx
@@ -20,6 +20,10 @@ class MyDocument extends Document<{ lang?: string }> {
             href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;500;600;800&display=swap"
             rel="stylesheet"
           />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+           />
         </Head>
         <body>
           <Main />
